333f7ed4e0c973833757ff3b1e95d911bd01b9f2
fun10th <106449262+fun10th@users.noreply.github.com>
2022-06-12 12:24:58 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ setup(
     name="racecar_gym",
     version="0.1",
     packages=find_packages(),
-    install_requires=['pybullet==3.1.7',
+    install_requires=['pybullet',
                       'scipy', # version automatically choosen w.r.t numpy
-                      'numpy==1.22.3',
-                      'gym==0.21.0',
-                      'yamldataclassconfig==1.5.0',
-                      'nptyping<2.0',
-                      'pettingzoo==1.18.1'
+                      'numpy',
+                      'gym=',
+                      'yamldataclassconfig',
+                      'nptyping',
+                      'pettingzoo'
                       ],
     author='Axel Brunnbauer',
     author_email='axel.brunnbauer@gmx.at',
